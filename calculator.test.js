b9d09ae9f1485406e5b6c9eb8eccf7b9b6f47e36
Ahmad Omar <ahmadomarns@gmail.com>
2023-09-04 00:01:32 +0300

    the test cases for calc function contains 19 test cases

---
+++ calculator.test.js
@@ -0,0 +1,82 @@
+// calculator.test.js
+const calc = require('./calculator');
+
+describe('Calculator', () => {
+  // Test case: Addition
+  it('should return the correct sum of two numbers', () => {
+    expect(calc(2, '+', 3)).toBe(5);
+  });
+
+   // Test case: Subtraction
+  it('should return the correct difference of two numbers', () => {
+    expect(calc(5, '-', 2)).toBe(3);
+  });
+
+   // Test case: Multiplication
+  it('should return the correct product of two numbers', () => {
+    expect(calc(4, '*', 6)).toBe(24);
+  });
+
+   // Test case: Division
+  it('should return the correct quotient of two numbers', () => {
+    expect(calc(10, '/', 2)).toBe(5);
+  });
+
+   // Test case: Division by zero
+  it('should throw an error when dividing by zero', () => {
+    expect(() => calc(6, '/', 0)).toThrow('Division by zero');
+  });
+
+   // Test case: Negative numbers
+  it('should handle negative numbers correctly', () => {
+    expect(calc(-8, '+', 5)).toBe(-3);
+  });
+
+  // Test case: Decimal numbers
+  it('should handle decimal numbers correctly', () => {
+    expect(calc(3.5, '*', 2)).toBe(7);
+  });
+
+   // Test case: Order of operations
+  it('should follow the correct order of operations', () => {
+    expect(calc(2, '+', 3, '*', 4)).toBe(14);
+  });
+  it('should follow the correct order of operations', () => {
+    expect(calc(3, '+', 5, '*', 9 , '-' , 2 , '/' ,1 )).toBe(46);
+  });
+
+   // Test case: Invalid operator
+  it('should throw an error for an invalid operator', () => {
+    expect(() => calc(5, '$', 3)).toThrow('Invalid operator');
+  });
+
+   // Test case: Invalid input type
+  it('should throw an error for invalid input types', () => {
+    expect(() => calc('2', '+', 3)).toThrow('Invalid input type');
+  });
+  it('should throw an error for invalid input numbers', () => {
+    expect(() => calc(2, 3)).toThrow('Invalid input');
+  });
+  it('should throw an error for invalid input numbers', () => {
+    expect(() => calc()).toThrow('Invalid input');
+  });
+  it('should return the correct sum of two numbers', () => {
+    expect(calc(2222, '+', 3)).toBe(3);
+  });
+  it('should throw an error for invalid input - mismatched operands', () => {
+    expect(() => calc(2, '+', 3, '+')).toThrow('Unexpected end of input');
+});
+it('should throw an error for invalid input - result is NaN with non-zero operands', () => {
+  expect(() => calc(5, '/', 'a')).toThrow('Invalid input');
+});
+it('should ignore the numbers greater than 1000', () => {
+  expect(calc(2222, '-', 3)).toBe(-3);
+});
+it('should ignore the numbers greater than 1000', () => {
+  expect(calc(2222, '*', 3)).toBe(3);
+});
+it('should ignore  numbers greater than 1000', () => {
+  expect(calc(1, '/', 1023)).toBe(1);
+});
+
+});
